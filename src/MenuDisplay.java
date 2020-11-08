7a3f2e3b74d9f448074f1df7d77d4ffc758c482c
nhhoang188 <nhhoang188@gmail.com>
2020-11-08 15:01:09 +0700

    btvn

---
+++ src/MenuDisplay.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+public class MenuDisplay {
+    public static void main(String[] args) {
+        int n = checkN();
+        NisPrime(n);
+        listNprime(n);
+        int[] arr = inputArrays(n);
+        listPrimeofArray(arr);
+        CheckNofArray(arr);
+    }
+
+    public static void CheckNofArray(int[] arr) {
+        int n = arr.length;
+        for (int i : arr) {
+            if (n != arr[i]) {
+                System.out.println("\n" + arr[i] + " Co Ton Tai Trong Mang Va Nam o Vi Tri Thu " + i);
+            } else {
+                System.out.println("\n" + n + " khong co trong mang." + arr[i]);
+            }
+            break;
+        }
+    }
+
+    public static void listPrimeofArray(int[] arr) {
+        System.out.print("\nCac So Nguyen To Trong Mang La : ");
+        for (int j : arr) {
+            if (isPrime(j)) {
+                System.out.print(j + ",");
+            }
+        }
+    }
+
+    public static int[] inputArrays(int n) {
+        int[] arr = new int[n];
+        int count = 0;
+        while (count < n) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Nhap Phan Tu Thu " + count + " Cua Mang: ");
+            arr[count] = input.nextInt();
+            count++;
+        }
+        System.out.print("Mang gom " + n + " Phan Tu La: ");
+        for (int a : arr) {
+            System.out.print(a + ",");
+        }
+        return arr;
+    }
+
+    public static void listNprime(int n) {
+        int count = 0, number = 2;
+        int[] arr = new int[n];
+        while (count < n) {
+            if (isPrime(number)) {
+                arr[count] = number;
+                count++;
+            }
+            number++;
+        }
+        System.out.print(n + " So Nguyen To Dau Tien La: ");
+        for (int a : arr) {
+            System.out.print(a + ",");
+        }
+        System.out.println();
+
+    }
+
+    public static int checkN() {
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Nhap So N le, N > 0 : ");
+            int n = input.nextInt();
+            if (inputN(n)) {
+                return n;
+            } else {
+                System.out.print("N Khong Thoa Man Moi Ban ");
+            }
+        } while (true);
+
+    }
+
+    public static void NisPrime(int n) {
+        if (isPrime(n)) {
+            System.out.println("So " + n + " La SNT");
+        } else {
+            System.out.println("So " + n + " Khong La SNT");
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            return check;
+        }
+    }
+
+    public static boolean inputN(int n) {
+        boolean b = true;
+        if (n % 2 == 1) {
+            return b;
+        } else {
+            return false;
+        }
+    }
+}
